585bf2cbcc41b194f2ffbe3f408e4f560b031b2d
h3pereu <dominikgryzbon@gmail.com>
2021-04-13 19:54:47 +0200

    fsa

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,11 +1,11 @@
 module.exports = (Discord, client, message) => {
-    const prefix = '!';
+    const prefix = process.env.PREFIX;
+
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const cmd = args.shift().toLowerCase();
 
-    const command = client.commands.get(cmd);
-
-    if(command) command.execute(message, args, cmd, client, Discord);
+    const command = client.commands.get(cmd) ||
+        client.commands.find(a => a.aliases && a.aliases.includes(cmd));
 }
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION" ]});
  
-const fs = require('fs');
- 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
@@ -10,4 +8,4 @@ client.events = new Discord.Collection();
     require(`./handlers/${handler}`)(client, Discord);
 })
 
-client.login(process.env.token);
+client.login('ODMwMTI5MzY1MjczODA0ODEw.YHCMNw.MKoTQXwuYkk3sj9teE4srYU0tYo');
